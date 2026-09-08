c08d8b4023b664e7452ea5abc2b7e571162aa241
Theo <zeldocto@gmail.com>
2026-09-08 15:24:32 -0700

    Set base path to '/moonshine-customs/' in config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import react from '@vitejs/plugin-react'
 const base = process.env.BASE_PATH ?? '/moonshine-customs/'
 
 export default defineConfig({
-  base,
+  base: '/moonshine-customs/',
   plugins: [react()],
   build: {
     outDir: 'dist',
